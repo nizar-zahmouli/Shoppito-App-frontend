a8fd235ad47809fea26ca1bbb7d7cca916d53807
Nizar <you@example.com>
2025-01-02 20:54:56 +0100

    frontend

--- src/redux/features/categoryAndBrand/categoryAndBrandService.js
+++ src/redux/features/categoryAndBrand/categoryAndBrandService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = "http://localhost:8000";
+const BACKEND_URL = "https://shopitoapp.onrender.com";
 export const API_URL = `${BACKEND_URL}/api/`;
 
 // Create Category
--- src/redux/features/coupon/couponService.js
+++ src/redux/features/coupon/couponService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BACKEND_URL = "http://localhost:8000";
+const BACKEND_URL = "https://shopitoapp.onrender.com";
 const API_URL = `${BACKEND_URL}/api/coupon/`;
 
 // Create New Coupon
