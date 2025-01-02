c920a3ccc3845710d1b9bc98d9e2db74ae64dc0f
Nizar <you@example.com>
2025-01-02 21:43:17 +0100

    frontend

--- src/redux/features/product/cartSlice.js
+++ src/redux/features/product/cartSlice.js
@@ -3,7 +3,7 @@ import { toast } from "react-toastify";
 import { getCartQuantityById } from "../../../utils";
 import cartService from "./cartService";
 
-const FRONTEND_URL = "https://shoppito-app.vercel.app";
+const FRONTEND_URL = " http://localhost:3000";
 
 // Apply discunt to cart
 function applyDiscount(cartTotalAmount, discountPercentage) {
